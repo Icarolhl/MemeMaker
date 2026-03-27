62d80c7d8ac5906eaf2ecc02d2878ffe79b91355
Icaro Lima <icarolima2882@gmail.com>
2026-03-27 10:36:44 -0300

    Ruff Formatting

--- src/mememaker/wsgi.py
+++ src/mememaker/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mememaker.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mememaker.settings")
 
 application = get_wsgi_application()
